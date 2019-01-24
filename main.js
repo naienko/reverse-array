ae2c5fa6e40b0e745932bb5eed7bbcc1e4c39a43
Panya Farnette <naienko@gmail.com>
2019-01-24 09:03:13 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// find a way to output the string "I am a rockstar ninja unicorn JS dev" from this array.
+
+// You will need to reverse the order of the strings in the array and then join them together into a single string.
+
+let hyperbole = ["dev", "JS", "unicorn", "ninja", "rockstar", "a", "am", "I"]
+
